5b2ef9cb435f0cf6c5eb2eb5d755c78db7e5211b
Ariya Panna <ariyapanna@outlook.com>
2024-05-23 17:19:35 +0700

    feat: added a handler for register a new user

--- Controllers/UserController/UserController.go
+++ Controllers/UserController/UserController.go
@@ -5,10 +5,13 @@ import (
 	"fmt"
 	"net/http"
 
+	"gymfinity-backend-api/Entities"
 	"gymfinity-backend-api/Library"
 	"gymfinity-backend-api/Models/UserModel"
+	"gymfinity-backend-api/Validator"
 
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
 )
 
 func Index(c *gin.Context) {
@@ -43,4 +46,29 @@ func Show(c *gin.Context) {
 	}
 
 	Library.ApiResponseSuccess(c, http.StatusOK, "Successfully fetch user's data", user);
+}
+
+func Create(c *gin.Context) {
+	var userData Entities.User;
+
+	if err := c.BindJSON(&userData); err != nil {
+		Library.ApiResponseError(c, http.StatusInternalServerError, err.Error());
+		return;
+	}
+
+	validate := validator.New();
+	Validator.RegisterCustomValidators(validate);
+	
+	if err := validate.Struct(userData); err != nil {
+		errors := err.(validator.ValidationErrors);
+		Library.ApiResponseError(c, http.StatusBadRequest, fmt.Sprintf("%v", errors));
+		return;
+	}
+
+	if err := UserModel.Create(&userData); err != nil {
+		Library.ApiResponseError(c, http.StatusInternalServerError, err.Error());
+		return;
+	}
+
+	Library.ApiResponseSuccess(c, http.StatusCreated, "A new user has been created", userData);
 }
