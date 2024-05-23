5bb3f7182e5b7708a5c5abd25fe271409952828a
Ariya Panna <ariyapanna@outlook.com>
2024-05-23 11:47:40 +0700

    feat: restructuring response format

--- Library/Response.go
+++ Library/Response.go
@@ -2,16 +2,21 @@ package Library
 
 import "github.com/gin-gonic/gin"
 
-type ResponseFormat struct {
+type SuccessResponseFormat struct {
 	StatusCode	int			`json:"statusCode"`
-	Error		string		`json:"error"`
+	Message		string		`json:"message"`
 	Data		interface{}	`json:"data"`
 }
 
-func ApiResponseSuccess(ctx *gin.Context, httpStatusCode int, data interface{}) {
-	ctx.JSON(httpStatusCode, ResponseFormat{StatusCode: httpStatusCode, Data: data});
+type ErrorResponseFormat struct {
+	StatusCode	int			`json:"statusCode"`
+	Error		string		`json:"error"`
+}
+
+func ApiResponseSuccess(ctx *gin.Context, httpStatusCode int, message string, data interface{}) {
+	ctx.JSON(httpStatusCode, SuccessResponseFormat{StatusCode: httpStatusCode, Message: message, Data: data});
 }
 
 func ApiResponseError(ctx *gin.Context, httpStatusCode int, err string) {
-	ctx.JSON(httpStatusCode, ResponseFormat{StatusCode: httpStatusCode, Error: err});
+	ctx.JSON(httpStatusCode, ErrorResponseFormat{StatusCode: httpStatusCode, Error: err});
 }
