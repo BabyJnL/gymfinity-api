356576d3a38fcb5e3687726da4cbd86b4d53f236
Ariya Panna <ariyapanna@outlook.com>
2024-05-23 17:16:52 +0700

    feat: added validation for each field

--- Entities/User.go
+++ Entities/User.go
@@ -2,15 +2,15 @@ package Entities
 
 type User struct {
 	UserID		int		`json:"userId"`
-	Firstname	string	`json:"firstname"`
-	Lastname	string	`json:"lastname"`
-	Gender		string	`json:"gender"`
-	Address		string	`json:"address"`
-	PhoneNumber	string	`json:"phoneNumber"`
-	Email		string	`json:"email"`
-	JoinDate	string	`json:"joinDate"`
-	Status		string	`json:"status"`
+	Firstname	string	`json:"firstname" validate:"required,max=64"`
+	Lastname	string	`json:"lastname" validate:"required,max=64"`
+	Gender		string	`json:"gender" validate:"required,validateGender"`
+	Address		string	`json:"address" validate:"required"`
+	PhoneNumber	string	`json:"phoneNumber" validate:"required,max=12"`
+	Email		string	`json:"email" validate:"required,email"`
+	JoinDate	string	`json:"joinDate" validate:"required"`
+	Status		string	`json:"status" validate:"required,validateStatus"`
 	ValidUntil	string	`json:"validUntil"`
-	Role		string	`json:"role"`
+	Role		string	`json:"role" validate:"required,validateRole"`
 	PhotoPath	string	`json:"photoPath"`
 }
