f81729a4b11f4127c7186a43d8ba78209541ea51
Ariya Panna <ariyapanna@outlook.com>
2024-05-23 10:06:46 +0700

    feat: added route for users endpoint

--- Router/Router.go
+++ Router/Router.go
@@ -1,6 +1,10 @@
 package Router
 
-import "github.com/gin-gonic/gin"
+import (
+	"gymfinity-backend-api/Controllers/UserController"
+
+	"github.com/gin-gonic/gin"
+)
 
 var trustedProxies = []string{
 	"127.0.0.1",
@@ -15,7 +19,8 @@ func SetupRoutes() {
 	// API Routes Group
 	api := router.Group("/api")
 	{
-		
+		// Users Routes
+		api.GET("/users", UserController.Index)
 	}
 
 	router.Run("localhost:8080")
