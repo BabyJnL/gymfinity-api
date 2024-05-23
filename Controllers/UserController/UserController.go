e883a26c366704bc7c5d6dbab0bc3a57b63544ea
Ariya Panna <ariyapanna@outlook.com>
2024-05-23 11:49:11 +0700

    feat: added handler for get user's data by its id

--- Controllers/UserController/UserController.go
+++ Controllers/UserController/UserController.go
@@ -1,22 +1,42 @@
 package UserController
 
 import (
+	"database/sql"
+	"fmt"
+	"net/http"
+
 	"gymfinity-backend-api/Library"
 	"gymfinity-backend-api/Models/UserModel"
-	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 func Index(c *gin.Context) {
-	roleParams := c.Query("role")
+	roleParams := c.Query("role");
 
 	users, err := UserModel.GetAll(&roleParams);
 
 	if err != nil {
-		Library.ApiResponseError(c, http.StatusInternalServerError, err.Error())
-		return
+		Library.ApiResponseError(c, http.StatusInternalServerError, err.Error());
+		return;
 	}
 
-	Library.ApiResponseSuccess(c, http.StatusOK, users)
+	Library.ApiResponseSuccess(c, http.StatusOK, "Successfully fetch all user's datas", users);
+}
+
+func Show(c *gin.Context) {
+	userId := Library.ParseInt(c.Param("id"));
+
+	user, err := UserModel.GetById(&userId);
+	if err != nil {
+		if err == sql.ErrNoRows {
+			Library.ApiResponseSuccess(c, http.StatusOK, fmt.Sprintf("No user found with id %d", userId), nil);
+		} else {
+			Library.ApiResponseError(c, http.StatusInternalServerError, err.Error());
+		}
+
+		return;
+	}
+
+	Library.ApiResponseSuccess(c, http.StatusOK, "Successfully fetch user's data", user);
 }
